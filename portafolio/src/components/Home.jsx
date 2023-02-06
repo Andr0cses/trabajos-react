a12a70b95030d622a82225b1b0ddf3c81fe26465
Andres <andrescamposs5c@gmail.com>
2023-02-06 17:01:32 -0300

    cambio Home

--- portafolio/src/components/Home.jsx
+++ portafolio/src/components/Home.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import {MdOutlineKeyboardArrowRight} from "react-icons/md"
+import FotoPf from "../assets/MIA.jpeg"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div name ="home" className='h-screen w-full bg-gradient-to-b from-slate-300 to-gray-800'>
+      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+
+        <div>
+
+          <h2> Hi I´m Andrés </h2>
+
+          <p>
+            texto aleatorio
+          </p>
+
+          <div>
+            <button>
+               Project
+               <span>
+                <MdOutlineKeyboardArrowRight />
+               </span>
+            </button>
+          </div>
+
+        </div>
+
+        <div>
+          <img src={FotoPf} alt= "" />
+        </div>
+
+      </div>
+    </div>
   )
 }
 
